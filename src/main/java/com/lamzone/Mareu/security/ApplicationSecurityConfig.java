3c3ea699d28b93131272ee2e09fbd423bb8aa7b6
papa bassirou ndiaye <pbnlgk@gmail.com>
2021-06-10 09:28:58 -0300

    white listed link

--- src/main/java/com/lamzone/Mareu/security/ApplicationSecurityConfig.java
+++ src/main/java/com/lamzone/Mareu/security/ApplicationSecurityConfig.java
@@ -13,6 +13,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/api/v1/meetings", "/api/v1/meetings/*")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
